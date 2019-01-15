d93f32e7e78d39ce7970b703cfaad3c5389d9b52
frquiros <franqm95@gmail.com>
2019-01-15 09:50:54 -0600

    added id to nav

--- Navigation.php
+++ Navigation.php
@@ -1,7 +1,7 @@
-<ul>
-  <li><a href="#" onclick="location.href='index.php'">Inicio</a></li>
-  <li><a href="#" onclick="location.href='ofertas.php'">Ofertas</a></li>
-  <li><a href="#" onclick="location.href='Consultas.php'">Consultas</a></li>
-  <li><a href="#" onclick="location.href='Perfil.php'">Perfil</a></li>
-  <li style="float:right"><a href="#" onclick="document.getElementById('LoginForm').style.display='block'" style="width:auto;">Iniciar Sesión</a></li>
-</ul>
+<ul  id="nav">
+    <li><a href="#" onclick="location.href='index.php'">Inicio</a></li>
+    <li><a href="#" onclick="location.href='ofertas.php'">Ofertas</a></li>
+    <li><a href="#" onclick="location.href='Consultas.php'">Consultas</a></li>
+    <li><a href="#" onclick="location.href='Perfil.php'">Perfil</a></li>
+    <li style="float:right"><a href="#" onclick="document.getElementById('LoginForm').style.display='block'" style="width:auto;">Iniciar Sesión</a></li>
+  </ul>
