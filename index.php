3d74219e170a2a7d0dfab74d073401ae6e8f4191
frquiros <franqm95@gmail.com>
2019-01-15 09:51:31 -0600

    Added Navigation.php

--- index.php
+++ index.php
@@ -13,13 +13,7 @@
 </head>
 
 <body>
-  <ul  id="nav">
-    <li><a href="#" onclick="location.href='index.php'">Inicio</a></li>
-    <li><a href="#" onclick="location.href='ofertas.php'">Ofertas</a></li>
-    <li><a href="#" onclick="location.href='Consultas.php'">Consultas</a></li>
-    <li><a href="#" onclick="location.href='Perfil.php'">Perfil</a></li>
-    <li style="float:right"><a href="#" onclick="document.getElementById('LoginForm').style.display='block'" style="width:auto;">Iniciar Sesión</a></li>
-  </ul>
+  <?php include "Navigation.php" ?>
 
   <div id="LoginForm" class="modal">
       <form class="modal-content" action="/action_page.php">
